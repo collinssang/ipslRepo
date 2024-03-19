463c24566db09e5d2ba18f51cea62f11836da5f7
kipkurui collins sang <kipkuruicollinssang@gmail.com>
2024-03-19 16:50:23 +0300

    security

--- src/main/java/com/ipsl/taskmanagement/controller/UserController.java
+++ src/main/java/com/ipsl/taskmanagement/controller/UserController.java
@@ -6,6 +6,9 @@ import com.ipsl.taskmanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +19,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthenticationManager authenticationManager) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping
     public ResponseEntity<?> login(@RequestBody User user) {
         User user1 = userService.login(user.getUsername(), user.getPassword());
         if (user1 != null) {
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body(user1);
-        }else{
+
+            Authentication authenticationRequest =
+                    UsernamePasswordAuthenticationToken.unauthenticated(user1.getUsername(), user1.getPassword());
+            Authentication authenticationResponse =
+                    this.authenticationManager.authenticate(authenticationRequest);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(authenticationResponse);
+        } else {
             ErrorResponses errorResponses = new ErrorResponses(HttpStatus.UNAUTHORIZED.value(), "Wrong Credentials Provided");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponses);
         }
